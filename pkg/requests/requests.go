d90155dbfe6b19f31a9e729ff551de89e02251bb
Alexamakans <fougner.alexander@gmail.com>
2021-07-15 10:11:34 +0200

    Applied code review suggestions

--- pkg/requests/requests.go
+++ pkg/requests/requests.go
@@ -8,14 +8,13 @@ import (
 	"net/url"
 )
 
-// ConstructGetURL Constructs a URL to use in a GET request.
-// Queries are passed as a map of string arrays
+// ConstructGetURL creates a GET request from a base URL, a map of query
+// parameters, and a formatted path.
 func ConstructGetURL(
 	rawURL string, queries map[string][]string, format string, values ...interface{}) (*url.URL, error) {
 
 	urlPath, err := url.Parse(rawURL)
 	if err != nil {
-		fmt.Printf("Unable to parse url %q", rawURL)
 		return nil, err
 	}
 
@@ -33,14 +32,9 @@ func GetAndParseJSON(result interface{}, user, token string, urlPath *url.URL) e
 	if err != nil {
 		return err
 	}
-
 	err = json.Unmarshal(body, &result)
-	if err != nil {
-		fmt.Println("Unable to unmarshal refs: ", err)
-		return err
-	}
 
-	return nil
+	return err
 }
 
 // GetAsString invokes a HTTP request with basic auth.
@@ -56,10 +50,8 @@ func GetAsString(user, token string, urlPath *url.URL) (string, error) {
 
 func getBodyFromRequest(user string, token string, urlPath *url.URL) ([]byte, error) {
 	url := urlPath.String()
-	fmt.Println("attempting to get from: ", url)
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		fmt.Println("Unable to get: ", err)
 		return []byte{}, err
 	}
 
@@ -67,15 +59,13 @@ func getBodyFromRequest(user string, token string, urlPath *url.URL) ([]byte, er
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Println("Unable to do request: ", err)
 		return []byte{}, err
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Unable to get. Status code: ", resp.StatusCode)
-		return []byte{}, err
+		return []byte{}, fmt.Errorf("unable to get. status code: %d", resp.StatusCode)
 	}
 
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
