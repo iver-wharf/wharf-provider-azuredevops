98fa9ada49d5ea71e3e2cc16d232a30bd09ae1a2
Alexamakans <fougner.alexander@gmail.com>
2021-09-09 16:37:07 +0200

    Moved test in azuredevops_test.go to internal/parseutil/util_test.go

--- internal/parseutil/util_test.go
+++ internal/parseutil/util_test.go
@@ -53,3 +53,40 @@ func TestSplitStringOnceRune(t *testing.T) {
 		})
 	}
 }
+
+func TestParseRepoRefParams(t *testing.T) {
+	var testCases = []struct {
+		name             string
+		wharfGroup       string
+		wharfProject     string
+		wantAzureOrg     string
+		wantAzureProject string
+		wantAzureRepo    string
+	}{
+		{
+			name:             "old v1 format",
+			wharfGroup:       "Org",
+			wharfProject:     "Proj",
+			wantAzureOrg:     "Org",
+			wantAzureProject: "Proj",
+			wantAzureRepo:    "",
+		},
+		{
+			name:             "new v2 format",
+			wharfGroup:       "Org/Proj",
+			wharfProject:     "Repo",
+			wantAzureOrg:     "Org",
+			wantAzureProject: "Proj",
+			wantAzureRepo:    "Repo",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			gotAzureOrg, gotAzureProject, gotAzureRepo := ParseRepoRefParams(tc.wharfGroup, tc.wharfProject)
+			assert.Equal(t, tc.wantAzureOrg, gotAzureOrg)
+			assert.Equal(t, tc.wantAzureProject, gotAzureProject)
+			assert.Equal(t, tc.wantAzureRepo, gotAzureRepo)
+		})
+	}
+}
